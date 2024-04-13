0ded938dc1f7453ddcc8b36b18026204ca735d29
sy-rex <sobolevkirill0@gmail.com>
2024-04-13 21:27:06 +0300

    Start create main window

--- project_for_smi/mainwindow.cpp
+++ project_for_smi/mainwindow.cpp
@@ -1,15 +1,36 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QPalette>
+#include <QColor>
+#include <QApplication>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setWindowFlags(Qt::Window | Qt::FramelessWindowHint | Qt::MSWindowsFixedSizeDialogHint);
+
+    // Создаем объект QColor для определения цвета с помощью шестнадцатеричного значения
+    QString hexColor = "#222338"; // это шестнадцатеричное представление синего цвета
+    QColor backgroundColor(hexColor);
+
+    // Создаем объект QPalette для установки цвета фона
+    QPalette palette;
+    palette.setColor(QPalette::Background, backgroundColor);
+
+    // Устанавливаем палитру для главного окна
+    setPalette(palette);
+
+    // Устанавливаем размеры окна
+    setGeometry(400,400,800, 600);
+
+    QPainterPath painPath;
+    painPath.addRoundedRect(rect(), 8, 8, Qt::AbsoluteSize);
+    this->setMask(painPath.toFillPolygon().toPolygon());
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
-
--- project_for_smi/mainwindow.h
+++ project_for_smi/mainwindow.h
@@ -1,7 +1,11 @@
+// mainwindow.h
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPainter>
+#include <QPainterPath>
+#include <QMouseEvent>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +19,37 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+    void mousePressEvent(QMouseEvent *event) override {
+        // Запомнить начальную позицию при нажатии на кнопку мыши
+        if (event->button() == Qt::LeftButton) {
+            m_dragging = true;
+            m_dragPosition = event->globalPos() - frameGeometry().topLeft();
+            event->accept();
+        }
+    }
+
+    void mouseMoveEvent(QMouseEvent *event) override {
+        // Перемещать окно при зажатой левой кнопке мыши
+        if (event->buttons() & Qt::LeftButton && m_dragging) {
+            move(event->globalPos() - m_dragPosition);
+            event->accept();
+        }
+    }
+
+    void mouseReleaseEvent(QMouseEvent *event) override {
+        // Остановить перемещение окна при отпускании кнопки мыши
+        if (event->button() == Qt::LeftButton) {
+            m_dragging = false;
+            event->accept();
+        }
+    }
+
 private:
     Ui::MainWindow *ui;
+    bool m_dragging = false;
+    QPoint m_dragPosition;
 };
+
 #endif // MAINWINDOW_H
+
